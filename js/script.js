bc28e5e91c0676086ab36e51336b9faabec168ca
roy_mral <roymral@gmail.com>
2024-04-03 12:10:43 -0600

    agregando giro a modelos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-AFRAME.registerComponent("arjs", {
-  init: function () {
-    this.el.sceneEl.addEventListener("loaded", () => {
-      console.log("Model loaded");
-    });
-  },
+window.addEventListener("load", function () {
+  const $containerGltf = document.querySelector(".container-gltf");
+  let rotateX = 60;
+
+  AFRAME.registerComponent("arjs", {
+    init: function () {
+      this.el.sceneEl.addEventListener("loaded", () => {
+        console.log("Model loaded");
+      });
+    },
+  });
+
+  const intervalId = setInterval(function () {
+    rotateX += 3;
+    $containerGltf.setAttribute("rotation", `${rotateX} 90 -90`);
+  }, 50);
 });
